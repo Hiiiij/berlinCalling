a8e39280f99c3c6eb4f6f428cd0cc8ef5c0ffd0b
beta <beta.nycz@icloud.com>
2021-05-14 16:40:16 +0200

    initial commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,22 @@
-import './ExploreContainer.css';
-
-interface ContainerProps { }
+import "./ExploreContainer.css";
+import { IonButton } from "@ionic/react";
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        Start with Ionic{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+          <IonButton color="lol">LOL</IonButton>
+        </a>
+      </p>
     </div>
   );
 };
